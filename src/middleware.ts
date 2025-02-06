1440c752b334426c5263584136baed0ecf8d4bb9
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-02-06 21:46:36 +0200

    fix: exclude SVG files from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -31,6 +31,6 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    '/((?!api/auth|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api/auth|_next/static|_next/image|favicon.ico|.*\.svg$).*)',
   ],
 }
