24cf7601a66d2faf418e882df016adb9c1835cbf
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-02-09 16:23:35 +0200

    refactor: remove deprecated storage implementation

--- src/lib/subscriptions/storage.ts
+++
@@ -1,6 +0,0 @@
-'use client';
-
-// This file is deprecated - use hooks/useSubscriptionStorage.ts instead
-// Keeping this file temporarily for reference, will be removed in the next cleanup
-
-export * from './hooks/useSubscriptionStorage';
