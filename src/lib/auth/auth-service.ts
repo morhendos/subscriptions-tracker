68a8dc2ea78b8b9058bb3dea2ac152b7226f9599
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-02-09 18:52:13 +0200

    chore: remove auth-service.ts

---
+++ src/lib/auth/auth-service.ts
@@ -0,0 +1,5 @@
+// This file has been moved to src/app/actions.ts
+// It exists only to prevent import errors during migration
+// TODO: Remove this file once all imports are updated
+
+export * from '@/app/actions';
